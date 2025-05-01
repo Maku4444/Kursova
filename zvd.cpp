71eae44f3c43a9205dc927e27cb30590c642fd80
Maku4444@ <dzulaymakar@gmail.com>
2025-05-01 14:27:46 +0300

    Заміна масиву на вектор

--- zvd.cpp
+++ zvd.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int field[7][10] = {
+vector<vector<int>> field = {
     { -2,  0,  0,  1,  0,  -1,  0, 1, 5, -2},
     { 1,  6,  3,  3,  2,  1,  4,  3,  2,  6},
     { 4,  6,  4,  0,  -2,  -2,  4,  5, 5, 4},
@@ -13,18 +13,19 @@ int field[7][10] = {
 };
 
 int main() {
-    for (int i = 0; i <= 6; i++) {
-        for (int j=0 ; j <= 9; j++) {
-            if(field[i][j]==-2){
-               cout<<"  ";
+    for (size_t i = 0; i < field.size(); ++i) {
+        for (size_t j = 0; j < field[i].size(); ++j) {
+            if(field[i][j] == -2) {
+                cout << "  ";
             }
-            else if(field[i][j]==-1){
-                cout<<"?"<< " ";
+            else if(field[i][j] == -1) {
+                cout << "? ";
             }
-            else{
-                cout << field[i][j] << " ";}
+            else {
+                cout << field[i][j] << " ";
+            }
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
