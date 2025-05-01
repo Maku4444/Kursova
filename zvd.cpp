9761971097ac92862ceded992a11fb7443da0d20
Maku4444@ <dzulaymakar@gmail.com>
2025-05-01 16:58:20 +0300

    modified:   zvd.cpp

--- zvd.cpp
+++ zvd.cpp
@@ -21,7 +21,34 @@ vector<pair<int, int>> needed_dominoes() {
     }
     return dominoes;
 }
+///-----------------
+bool valid(int r, int c) {
+    return r >= 0 && r < field.size() && c >= 0 && c < field[r].size() && field[r][c] != -2;
+}
 
+bool can_be_domino(int r1, int c1, int r2, int c2, int val1, int val2) {
+
+    if (!valid(r1, c1) || !valid(r2, c2)) {
+        return false;
+    }
+
+    if (!((r1 == r2 && abs(c1 - c2) == 1) || (c1 == c2 && abs(r1 - r2) == 1))) {
+        return false;
+    }
+
+    int field1 = field[r1][c1];
+    int field2 = field[r2][c2];
+
+    return (field1 == -1 && field2 == -1) ||
+           (field1 == -1 && field2 == val1) ||
+           (field1 == -1 && field2 == val2) ||
+           (field1 == val1 && field2 == -1) ||
+           (field1 == val2 && field2 == -1) ||
+           (field1 == val1 && field2 == val2) ||
+           (field1 == val2 && field2 == val1);
+}
+///----------------
+ 
 int main() {
     ///--------------
     for (size_t i = 0; i < field.size(); ++i) {
